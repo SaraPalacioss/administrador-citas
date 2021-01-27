ab61c6c917d788f1e45d75b8fd6e0e206ec92786
sarastackhouse <sarapalacios7@gmail.com>
2021-01-27 20:20:54 +0100

    validation donde

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,16 +1,67 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Formulario = () => {
+
+// Crear State de citas
+
+const [cita, actualizarCita] = useState({
+    mascota: '',
+    propietario: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+})
+
+const [error, actualizarError] = useState(false)
+// Función que se ejecuta cada vez que el usuario actualiza el imput
+
+const actualizarState =e=> {
+    actualizarCita({
+        ...cita,
+        [e.target.name]: e.target.value
+    })
+}
+
+// Extraer los valores
+
+const {mascota, propietario, fecha, hora, sintomas} = cita;
+
+// Cuando el usuario presiona agregar cita
+const submitCita =e=>{
+    e.preventDefault();
+
+    // Validar
+    if(mascota.trim() === '' || propietario.trim === '' || fecha.trim === ''|| hora.trim === '' || sintomas.trim === ''){
+        console.log('Hay un error');
+        actualizarError(true)
+        return;
+    }
+
+    // Asignar un ID
+
+    // Crear cita
+
+    // Reiniciar form
+}
+
+
   return (
     <Fragment>
       <h2>Crear Cita</h2>
-      <form>
+
+      {error ? <p className="alerta-error">Todos los campos son obligatorios</p>
+      : null}
+      <form
+        onSubmit={submitCita}
+      >
         <label>Nombre Mascota</label>
         <input
           type="text"
           name="mascota"
           className="u-full-width"
           placeholder="Nombre Mascota"
+          onChange={actualizarState}
+          value={mascota}
         />
         <label>Nombre Dueño</label>
         <input
@@ -18,25 +69,41 @@ const Formulario = () => {
           name="propietario"
           className="u-full-width"
           placeholder="Nombre Dueño de la mascota"
+          onChange={actualizarState}
+          value={propietario}
+
         />
         <label>Fecha</label>
         <input 
             type="date" 
             name="fecha" 
-            className="u-full-width" />
+            className="u-full-width"
+            onChange={actualizarState}
+            value={fecha}
+
+        />
+            
         <label>Hora</label>
         <input 
             type="time" 
             name="hora" 
-            className="u-full-width" />
+            className="u-full-width"
+            onChange={actualizarState}
+            value={hora}
+
+        />
         <label>Síntomas</label>
         <textarea 
-            name="fecha" 
-            className="u-full-width">
+            name="sintomas" 
+            className="u-full-width"
+            onChange={actualizarState}
+            value={sintomas}
+        >
         </textarea>
         <button
             type="submit"
             className="u-full-width button-primary"
+            onChange={actualizarState}
         >
         Agregar Cita
         </button>
